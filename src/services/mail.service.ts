a4e5c0c4b98206ba3ae41f4cecd1edf0dd714a65
Olamide <simonadepetoye@gmail.com>
2024-01-07 15:02:53 +0100

    pushing to test if issue is from linux

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -3,7 +3,7 @@ import AppError from '../utils/appError'
 
 let transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  secure: true,
+  secure: false,
   port: process.env.EMAIL_PORT,
   logger: true,
   debug: true,
