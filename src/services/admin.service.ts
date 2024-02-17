cbc82a286be8af3243b93712f7d4fd9301a6b800
daniel-dunsin <adejaredaniel12@gmail.com>
2024-02-17 18:45:16 +0100

    fix: populate student and classes field in upcoming classes endpoint

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,47 +1,50 @@
-import { Prisma } from '@prisma/client'
-import prisma from '../utils/prisma'
-import { hashPassword } from '../utils/password.manager'
+import { Prisma } from "@prisma/client";
+import prisma from "../utils/prisma";
+import { hashPassword } from "../utils/password.manager";
 
 export const addAdmin = async (data: Prisma.adminCreateInput) => {
   try {
-    data.password = await hashPassword(data.password)
+    data.password = await hashPassword(data.password);
     return await prisma.admin.create({
       data,
-    })
+    });
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
-export const updateAdmin = async (id: string, data: Partial<Prisma.adminUpdateInput>) => {
+export const updateAdmin = async (
+  id: string,
+  data: Partial<Prisma.adminUpdateInput>
+) => {
   try {
     if (data.password) {
-      data.password = await hashPassword(data.password as string)
+      data.password = await hashPassword(data.password as string);
     }
-    return await prisma.admin.update({ where: { id }, data })
+    return await prisma.admin.update({ where: { id }, data });
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 export const deleteAdmin = async (id: string) => {
   try {
-    return await prisma.admin.delete({ where: { id } })
+    return await prisma.admin.delete({ where: { id } });
   } catch (error) {}
-}
+};
 
 export const checkIfSuperAdmin = async (id: string) => {
   const admin = await prisma.admin.findUnique({
     where: { id },
-  })
+  });
 
-  return admin?.isAdmin
-}
+  return admin?.isAdmin;
+};
 
 export const findAdminByDetails = (data: any) => {
-  const { name, phoneNumber } = data
-  return prisma.admin.findFirstOrThrow({ where: { name, phoneNumber } })
-}
+  const { name, phoneNumber } = data;
+  return prisma.admin.findFirstOrThrow({ where: { name, phoneNumber } });
+};
 
 export const getUpcomingClasses = () => {
   return prisma.student_knowledge.findMany({
@@ -50,8 +53,12 @@ export const getUpcomingClasses = () => {
         gte: new Date(),
       },
     },
-    orderBy: {
-      date: 'asc',
+    include: {
+      student: true,
+      classes: true,
     },
-  })
-}
+    orderBy: {
+      date: "asc",
+    },
+  });
+};
