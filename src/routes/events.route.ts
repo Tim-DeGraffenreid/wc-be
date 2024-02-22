883a74e166c30430b024207ecfd8d89f3fe27814
Olamide <simonadepetoye@gmail.com>
2024-02-22 01:07:02 +0100

    feat:events management

--- src/routes/events.route.ts
+++ src/routes/events.route.ts
@@ -2,10 +2,14 @@ import express from 'express'
 import { deserializeUser } from '../middlewares/deserializeUser'
 import { requireUser } from '../middlewares/requireUser'
 import { createEventhandler, getEventsHandler } from '../controllers/events.controller'
+import { validate } from '../middlewares/validate'
+import { createEventSchema } from '../schemas/events.schema'
 
 const router = express.Router()
 
-router.route('/new/:classId').post(deserializeUser, requireUser, createEventhandler)
+router
+  .route('/new/:classId')
+  .post(deserializeUser, requireUser, validate(createEventSchema), createEventhandler)
 router.route('/').get(getEventsHandler)
 
 export default router
--- src/schemas/events.schema.ts
+++ src/schemas/events.schema.ts
@@ -1,7 +1,23 @@
-import { object, string } from "zod";
+import { object, string } from 'zod'
 
 export const createEventSchema = object({
-    body: object({
-      
+  params: object({
+    classId: string({
+      required_error: 'Class id is required',
     }),
-  })
+  }),
+  body: object({
+    start_time: string({
+      required_error: 'Start time is required',
+    }).datetime('Start time must be in the future'),
+    end_time: string({
+      required_error: 'End time is required',
+    }).datetime('End time must be after start time'),
+    location: string({
+      required_error: 'Location is required',
+    }),
+    instructor: string({
+      required_error: 'Instructor is required',
+    }),
+  }),
+})
